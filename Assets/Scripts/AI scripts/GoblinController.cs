b248e9d8c45c8d45a512a82cdc7e6a1054c8ce67
ivissivi <ivis31121999@gmail.com>
2022-06-07 14:32:35 +0300

    Fixed the bug where AI doesn't stop after coming in contact with player

--- Assets/Scripts/AI scripts/GoblinController.cs
+++ Assets/Scripts/AI scripts/GoblinController.cs
@@ -5,6 +5,7 @@ using UnityEngine.AI;
 
 public class GoblinController : MonoBehaviour
 {
+    [SerializeField] private float stoppingDistance = 1.2f;
     private NavMeshAgent agent = null;
     private Animator animator = null;
     [SerializeField] private Transform target;
@@ -18,10 +19,27 @@ public class GoblinController : MonoBehaviour
     private void MoveToPlayer()
     {
         agent.SetDestination(target.position);
-        animator.SetFloat("Velocity", 0.4f, 0.1f, Time.deltaTime);
 
         float distanceToPlayer = Vector3.Distance(transform.position, target.position);
 
+        if(distanceToPlayer >= agent.stoppingDistance) 
+        {
+            animator.SetFloat("Velocity", 0.5f, 0.1f, Time.deltaTime);
+        }
+
+        if(distanceToPlayer <= agent.stoppingDistance)
+        {
+            animator.SetFloat("Velocity", 0.1f);
+        }
+    }
+
+    private void RotateToPlayer() 
+    {
+        //transform.LookAt(target);
+
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = rotation;
     }
 
     // Update is called once per frame
