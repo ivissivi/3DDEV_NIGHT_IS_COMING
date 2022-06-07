cbe0d89deee53375428d320cbae92fda4c532b23
ivissivi <ivis31121999@gmail.com>
2022-06-07 11:47:46 +0300

    Removed example assets

--- Assets/Scripts/AI scripts/GoblinController.cs
+++ Assets/Scripts/AI scripts/GoblinController.cs
@@ -6,16 +6,22 @@ using UnityEngine.AI;
 public class GoblinController : MonoBehaviour
 {
     private NavMeshAgent agent = null;
+    private Animator animator = null;
     [SerializeField] private Transform target;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void MoveToPlayer()
     {
         agent.SetDestination(target.position);
+        animator.SetFloat("Velocity", 0.4f, 0.1f, Time.deltaTime);
+
+        float distanceToPlayer = Vector3.Distance(transform.position, target.position);
+
     }
 
     // Update is called once per frame
