69593c8781ce279b44c6c68c71dd6975aea8ba4b
ivissivi <ivis31121999@gmail.com>
2022-06-09 02:10:55 +0300

    Added pause screen

--- Assets/Scripts/Player scripts/UIController.cs
+++ Assets/Scripts/Player scripts/UIController.cs
@@ -7,16 +7,62 @@ public class UIController : MonoBehaviour
 {
     [SerializeField] private GameObject hudUI = null;
     [SerializeField] private GameObject endUI = null;
+    [SerializeField] private GameObject pauseUI = null;
+
+    private bool isPaused = false;
+
+    private CameraController cameraController = null; 
+    [SerializeField] private PlayerStats stats = null;
 
     private void Start()
     {
         SetHUD(true);
+        cameraController = GetComponentInChildren<CameraController>();
+        stats = GetComponent<PlayerStats>();
     }   
 
     public void SetHUD(bool state)
     {
             hudUI.SetActive(state);
             endUI.SetActive(!state);
+            if(!stats.IsDead())
+            {
+                pauseUI.SetActive(!state);
+            }
+    }
+
+    public void SetPause(bool state)
+    {
+        pauseUI.SetActive(state);
+        hudUI.SetActive(!state);
+
+        if(state)
+        {
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        isPaused = state;
+    }
+
+    private void Update()
+    {
+        if(!stats.IsDead())
+        {
+            if(Input.GetKeyDown(KeyCode.Escape) && !isPaused)
+            {
+                SetPause(true);
+            }
+            else if(Input.GetKeyDown(KeyCode.Escape) && isPaused)
+            {
+                SetPause(false);
+            }
+        }
     }
 
     public void Restart()
